468fd402fd7766dc5b15795e31d6572999be805f
Minh Le <lenguyetminh1998@gmail.com>
2017-03-19 23:14:12 -0400

    Delete examplejs.js

--- examplejs.js
+++
@@ -1,6 +0,0 @@
-var ourRequest = new XMLHttpRequest();
-ourReqest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-ourReqest.onload = function(){
-  console.log(ourRequest.responseText);
-}
-ourRequest.send();
