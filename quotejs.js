c50ca577b1f37a592114280314f5efe07bbb9aaa
Minh Le <lenguyetminh1998@gmail.com>
2017-03-21 14:31:02 -0400

    Display JSON data to HTML div

--- quotejs.js
+++ quotejs.js
@@ -1,9 +1,20 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'http://quotes.stormconsultancy.co.uk/quotes.json');
-request.onload = function(){
-  var data = JSON.parse(request.responseText);
+
+var display = document.getElementById("quote-display");
+var btn = document.getElementById("newquote");
+
+btn.addEventListener("click", function(){
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://quotes.stormconsultancy.co.uk/quotes.json');
+  request.onload = function(){
+    var data = JSON.parse(request.responseText);
+    renderHTML(data);
+  }
+  request.send();
+});
+
+
+function renderHTML(data){
   var x =  Math.floor((Math.random()*40) + 1);
-  console.log(data[x].author)
-  alert(x);
+  var htmlString = "<p>" + data[0].author + "is the author" + "</p>";
+  display.insertAdjacentHTML('beforeend', htmlString); 
 }
-request.send();
