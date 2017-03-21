4d931188ee3b8188bb50e04f1bdc55123d107669
Minh Le <lenguyetminh1998@gmail.com>
2017-03-21 14:12:29 -0400

    Add in random number generator

--- quotejs.js
+++ quotejs.js
@@ -1,6 +1,9 @@
 var request = new XMLHttpRequest();
 request.open('GET', 'http://quotes.stormconsultancy.co.uk/quotes.json');
 request.onload = function(){
-  console.log(request.responseText);
+  var data = JSON.parse(request.responseText);
+  var x =  Math.floor((Math.random()*40) + 1);
+  console.log(data[x].author)
+  alert(x);
 }
 request.send();
